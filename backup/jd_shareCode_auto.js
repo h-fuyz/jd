be13184022345a48cbc6102b85dc1dfb458012a6
xujie <348807605@qq.com>
2021-01-18 14:13:38 +0800

    tijiao

--- backup/jd_shareCode_auto.js
+++ backup/jd_shareCode_auto.js
@@ -1,7 +1,7 @@
 const $ = new Env("京东分享码");
-const notify = $.isNode() ? require("./jd_script/sendNotify") : "";
+const notify = $.isNode() ? require("./sendNotify") : "";
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require("./jd_script/jdCookie.js") : "";
+const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 const JD_API_HOST = "https://api.m.jd.com/client.action";
 let cookiesArr = [],
     cookie = "";
